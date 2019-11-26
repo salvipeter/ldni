696fa2b29677a071c6e15e2d60866be0416f150e
Peter Salvi <vukung@yahoo.com>
2019-11-26 22:15:07 +0100

    Better QEF

--- cell-point.cc
+++ cell-point.cc
@@ -7,16 +7,24 @@ using namespace Geometry;
 Point3D cellPoint(const std::vector<Point3D> &points, const std::vector<Vector3D> &normals,
                   const Point3D &cell_min, const Point3D &cell_max) {
   size_t m = points.size();
+  Point3D centroid(0, 0, 0);
+  for (const auto &p : points)
+    centroid += p;
+  centroid /= m;
+
   if (m >= 3) {
     // Setup QEF
-    Eigen::MatrixXd A(m, 3);
-    Eigen::VectorXd b(m);
+    Eigen::MatrixXd A(m + 3, 3);
+    Eigen::VectorXd b(m + 3);
     for (size_t i = 0; i < m; ++i) {
       const auto &n = normals[i];
       for (size_t j = 0; j < 3; ++j)
         A(i, j) = n[j];
       b(i) = n * points[i];
     }
+    A(  m  , 0) = 1; A(  m  , 1) = 0; A(  m  , 2) = 0; b(  m  ) = centroid[0];
+    A(m + 1, 0) = 0; A(m + 1, 1) = 1; A(m + 1, 2) = 0; b(m + 1) = centroid[1];
+    A(m + 2, 0) = 0; A(m + 2, 1) = 0; A(m + 2, 2) = 1; b(m + 2) = centroid[2];
 
     // Solve
     Eigen::Vector3d x = A.colPivHouseholderQr().solve(b);
@@ -30,8 +38,5 @@ Point3D cellPoint(const std::vector<Point3D> &points, const std::vector<Vector3D
   }
 
   // Fall back to the mass center of the crossing points
-  Point3D result(0, 0, 0);
-  for (const auto &p : points)
-    result += p;
-  return result / m;
+  return centroid;
 }
